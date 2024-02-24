06b60800bd21ae15531c43f05bcc09f6ced22965
div11R <divyanshudw64@gmail.com>
2024-02-25 01:15:00 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// Create a scene
+const scene = new THREE.Scene();
+const loader = new GLTFLoader();
+
+let mixer;
+loader.load(
+    './footballground2.glb', // or .gltf
+    function (gltf) {
+        scene.add(gltf.scene);
+    },
+    undefined,
+    function (error) {
+        console.error('Error loading GLTF/GLB file:', error);
+    }
+);
+
+const box = document.querySelector(".box");
+console.log(box);
+
+
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z=-40;
+camera.position.y=40;
+camera.lookAt(0,50,50)
+
+
+
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('container').appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
+
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); 
+
+
+
+
+const clock = new THREE.Clock();
+
+function animate() {
+    requestAnimationFrame(animate);
+
+
+        controls.update();
+
+    renderer.render(scene, camera);
+}
+animate();
